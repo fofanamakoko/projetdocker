3639f8ad1c43f9b069be9f905897d7bde4e2abb5
fofona <leilafofana.makoko@ynov.com>
2021-02-25 00:20:23 +0100

    seconde commit

---
+++ test/test1.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Test  fonctionne', function() {
+  
+    it('should do something', function() {
+         var a = 3;
+      assert.equal(a*2, 6, 'la multiplication ne fonctionne pas');
+      
+    });
+  });
